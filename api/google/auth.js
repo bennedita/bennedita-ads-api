a501240518ff56f70bc76a9d47663487ecd0780f
bennedita <agenciabennedita@gmail.com>
2026-02-07 14:22:29 -0300

    Create auth.js

---
+++ api/google/auth.js
@@ -0,0 +1,33 @@
+import fetch from "node-fetch";
+
+export async function getAccessToken() {
+  const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!refreshToken) {
+    throw new Error("REFRESH TOKEN não configurado");
+  }
+
+  const response = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      refresh_token: refreshToken,
+      grant_type: "refresh_token",
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.error("Erro ao gerar access token:", data);
+    throw new Error("Falha ao gerar access token");
+  }
+
+  return data.access_token;
+}
