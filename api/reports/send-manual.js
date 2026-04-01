aa578c0b7fd12723a2a88346f326ddc14ccd92e4
bennedita <agenciabennedita@gmail.com>
2026-04-01 12:13:14 -0300

    Update send-manual.js

--- api/reports/send-manual.js
+++ api/reports/send-manual.js
@@ -1,3 +1,15 @@
+export default async function handler(req, res) {
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  // ... resto do código
+}
 import { Resend } from "resend";
 import { neon } from "@neondatabase/serverless";
 
